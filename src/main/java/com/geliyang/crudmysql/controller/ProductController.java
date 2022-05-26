3e63bfd783bab48b33b5b65c3b9b9c8b2f35ad8f
Geli Yang <gyang5@villanova.edu>
2022-05-26 16:20:17 -0400

    implemented Get, PUT and DELETE mapping

--- src/main/java/com/geliyang/crudmysql/controller/ProductController.java
+++ src/main/java/com/geliyang/crudmysql/controller/ProductController.java
@@ -4,10 +4,10 @@ package com.geliyang.crudmysql.controller;
 import com.geliyang.crudmysql.entity.Product;
 import com.geliyang.crudmysql.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -25,9 +25,32 @@ public class ProductController {
         return productService.saveProducts(products);
     }
 
+    @GetMapping("/Products")
+    public List<Product> findAllProducts(){
+        return productService.getProducts();
+    }
+
+    @GetMapping("Product/{id}")
+    public Optional<Product> findProductById(@PathVariable int id){
+        return productService.getProductById(id);
+    }
 
 
-    
+    @GetMapping("Product/{name}")
+    public Product findProductByName(@PathVariable String name){
+        return productService.getProductByName(name);
+    }
+
+
+    @PutMapping("/update")
+    public Product updateProduct(@RequestBody Product product){
+        return productService.updateProduct(product);
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteProduct(@PathVariable int id){
+        return productService.deleteProduct(id);
+    }
 
 
 }
