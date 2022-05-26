07d2fd5cea05f4c5c17386a2fa2133e678ec2328
Geli Yang <gyang5@villanova.edu>
2022-05-26 14:56:49 -0400

    initial commit
    commit initial spring initializr package

---
+++ src/main/java/com/geliyang/crudmysql/CruDmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.geliyang.crudmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDmysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruDmysqlApplication.class, args);
+    }
+
+}
