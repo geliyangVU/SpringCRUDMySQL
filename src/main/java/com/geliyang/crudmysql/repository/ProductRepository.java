4d7bb8696a6eb18bda9afcaff1ae49fe5d8fe332
Geli Yang <gyang5@villanova.edu>
2022-05-26 15:14:48 -0400

    extend JPArepository in ProductRepository.java

---
+++ src/main/java/com/geliyang/crudmysql/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.geliyang.crudmysql.repository;
+
+import com.geliyang.crudmysql.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+}
